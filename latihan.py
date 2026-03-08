ebce19bf8018fd5fc9862b6ee613ceb1b66bcb51
Faiz Hisyam Rasyid Harahap <Faizhi367@gmail.com>
2026-03-09 00:01:11 +0700

    selesai

--- latihan.py
+++ latihan.py
@@ -7,6 +7,19 @@ NPM: 25782068
 Tanggal Pembaruan: 8 Maret 2026
 """
 def avg(data):
+    """
+    Hitung nilai rata-rata dari list data yang diberikan.
+
+    Args:
+        data (list): List berisi angka (int atau float).
+
+    Returns:
+        float: Hasil rata-rata, atau 0.0 jika list kosong.
+
+    Example:
+        >>> avg([10, 20, 30])
+        20.0
+    """
     total = 0
     for d in data:
         total += d
