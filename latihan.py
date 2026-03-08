006a5f13b382661a992998299f5599ffebea520d
Faiz Hisyam Rasyid Harahap <Faizhi367@gmail.com>
2026-03-08 23:57:31 +0700

    pydoc

--- latihan.py
+++ latihan.py
@@ -1,3 +1,11 @@
+"""
+Nama File: latihan.py
+Tujuan: Menghitung rata-rata nilai dari sebuah list angka.
+Fitur Utama: Validasi data kosong dan kalkulasi rata-rata.
+Penulis: Faiz Hisyam Rasyid Harahap
+NPM: 25782068
+Tanggal Pembaruan: 8 Maret 2026
+"""
 def avg(data):
     total = 0
     for d in data:
